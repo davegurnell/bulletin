8b884e836a9727a64329611c991d8f30e00090fe
Dave Gurnell <dave@untyped.com>
2016-03-21 12:18:54 +0000

    Better support for custom Merge instances

--- src/main/scala/bulletin/Merge.scala
+++ src/main/scala/bulletin/Merge.scala
@@ -7,47 +7,47 @@ import shapeless.labelled._
 @implicitNotFound("Cannot update a ${A} with a ${B}. Check the field types match up, or manually create a Merge instance for these types.")
 trait Merge[A, B] extends ((A, B) => A)
 
-object Merge extends MergeConstructors
-  with MergeImplicits
+object Merge extends MergeFunctions with MergeImplicits
 
-private[bulletin] trait MergeConstructors {
+private[bulletin] trait MergeFunctions {
   /** Summon a [[Merge]] instance from implicit scope. */
   def apply[A, B](implicit merge: Merge[A, B]): Merge[A, B] =
     merge
 
   /** Create a [[Merge]] instance from a function. */
-  def create[A, B](func: (A, B) => A): Merge[A, B] =
+  def instance[A, B](func: (A, B) => A): Merge[A, B] =
     new Merge[A, B] {
       def apply(a: A, b: B): A = func(a, b)
     }
 }
 
-private[bulletin] trait MergeImplicits {
-  self: MergeConstructors =>
+private[bulletin] trait MergeImplicits extends LowPriorityMergeImplicits with HighPriorityMergeImplicits
 
-  /** Merge two non-empty HLists. */
-  implicit def requiredMerge[Name <: Symbol, HeadA, TailA <: HList, HeadB, TailB <: HList](
-    implicit
-    witness: Witness.Aux[Name],
-    evidence: HeadB =:= HeadA,
-    tailMerge: Merge[TailA, TailB]
-  ): Merge[FieldType[Name, HeadA] :: TailA, FieldType[Name, HeadB] :: TailB] =
-    create[FieldType[Name, HeadA] :: TailA, FieldType[Name, HeadB] :: TailB] { (a, b) =>
-      val head: FieldType[Name, HeadA] = field[Name](b.head)
-      val tail: TailA = tailMerge(a.tail, b.tail)
-      head :: tail
+private[bulletin] trait LowPriorityMergeImplicits {
+  implicit def requiredMerge[A, B](
+    implicit ev: B <:< A
+  ): Merge[A, B] =
+    Merge.instance[A, B] { (original, update) =>
+      update
     }
 
+  implicit def optionalMerge[A]: Merge[A, Option[A]] =
+    Merge.instance[A, Option[A]] { (original, update) =>
+      update getOrElse original
+    }
+}
+
+private[bulletin] trait HighPriorityMergeImplicits {
   /** Merge two non-empty HLists. */
-  implicit def optionalMerge[Name <: Symbol, HeadA, TailA <: HList, HeadB, TailB <: HList](
+  implicit def listMerge[Name <: Symbol, HeadA, TailA <: HList, HeadB, TailB <: HList](
     implicit
     witness: Witness.Aux[Name],
-    evidence: HeadB <:< Option[HeadA],
-    tailMerge: Merge[TailA, TailB]
+    headMerge: Merge[HeadA, HeadB],
+    tailMerge: Lazy[Merge[TailA, TailB]]
   ): Merge[FieldType[Name, HeadA] :: TailA, FieldType[Name, HeadB] :: TailB] =
-    create[FieldType[Name, HeadA] :: TailA, FieldType[Name, HeadB] :: TailB] { (a, b) =>
-      val head: FieldType[Name, HeadA] = field[Name](b.head getOrElse a.head)
-      val tail: TailA = tailMerge(a.tail, b.tail)
+    Merge.instance[FieldType[Name, HeadA] :: TailA, FieldType[Name, HeadB] :: TailB] { (a, b) =>
+      val head: FieldType[Name, HeadA] = field[Name](headMerge(a.head, b.head))
+      val tail: TailA = tailMerge.value(a.tail, b.tail)
       head :: tail
     }
 
@@ -55,17 +55,17 @@ private[bulletin] trait MergeImplicits {
   implicit def skipMerge[Name <: Symbol, HeadA, TailA <: HList, ListB <: HList](
     implicit
     witness: Witness.Aux[Name],
-    tailMerge: Merge[TailA, ListB]
+    tailMerge: Lazy[Merge[TailA, ListB]]
   ): Merge[FieldType[Name, HeadA] :: TailA, ListB] =
-    create[FieldType[Name, HeadA] :: TailA, ListB] { (a, b) =>
+    Merge.instance[FieldType[Name, HeadA] :: TailA, ListB] { (a, b) =>
       val head: FieldType[Name, HeadA] = a.head
-      val tail: TailA = tailMerge(a.tail, b)
+      val tail: TailA = tailMerge.value(a.tail, b)
       head :: tail
     }
 
   /** Merge two empty HLists. */
   implicit val nilMerge: Merge[HNil, HNil] =
-    create[HNil, HNil] { (a, b) =>
+    Merge.instance[HNil, HNil] { (a, b) =>
       HNil
     }
 
@@ -76,7 +76,7 @@ private[bulletin] trait MergeImplicits {
     genB: LabelledGeneric.Aux[B, HListB],
     merge: Merge[HListA, HListB]
   ): Merge[A, B] =
-    create[A, B] { (a, b) =>
+    Merge.instance[A, B] { (a, b) =>
       genA.from(merge(genA.to(a), genB.to(b)))
     }
 }
